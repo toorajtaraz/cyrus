9cb0e84c5ffc8f08524e48454f0e06cee48c7330
Tooraj Taraz <tooraj.info@gmail.com>
2022-06-12 12:00:50 +0430

    testing upload and download functionality of the opencv cuda

--- main.cpp
+++ main.cpp
@@ -18,5 +18,16 @@ int main()
     bool _isd_evice_compatible = _deviceInfo.isCompatible();
     cout << "CUDA Device(s) Compatible: " << _isd_evice_compatible << endl;
     cout << "Hello, World!" << endl;
+    //open 1.jpg and upload it to gpu then download it and show it
+    cv::Mat _src_img = cv::imread("E:\\university\\Projects\\cyrus\\data\\1.jpg");
+    cv::Mat _dst_img;
+    cv::cuda::GpuMat _src_gpu_img;
+    cv::cuda::GpuMat _dst_gpu_img;
+    _src_gpu_img.upload(_src_img);
+    cv::cuda::add(_src_gpu_img, _src_gpu_img, _dst_gpu_img);
+    _dst_gpu_img.download(_dst_img);
+    cv::imshow("src_img", _src_img);
+    cv::imshow("dst_img", _dst_img);
+    cv::waitKey(0);
     return 0;
 }
