0fb2f21a921867c10a1b4614bc4d51f086518825
Tooraj Taraz <tooraj.info@gmail.com>
2022-07-04 10:23:40 +0430

    adding helper macros for cleaner code

--- headers/helpers.h
+++ headers/helpers.h
@@ -1,4 +1,4 @@
-//adding def gaurds
+// adding def gaurds
 #ifndef HELPERS_H
 #define HELPERS_H
 
@@ -16,32 +16,125 @@
 #define RGB_CHANNELS 3
 #define MAX_PIXEL_VAL 255
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/cudaarithm.hpp>
 #include <cuda.h>
+#include <opencv2/cudaarithm.hpp>
+#include <opencv2/opencv.hpp>
 
-//device function that extracts histogram of an image on gpu
-__device__ void helper_extract_histogram_rgb(const uchar* img,  int *count, int x_start, int x_end, int y_start, int y_end, int width, int height, int steps, short channel, short channels_c = 3, int *histt = NULL, int sw = 1);
-__device__ void helper_extract_histogram(const uchar* img, int *count, int x_start, int x_end, int y_start, int y_end, int width, int height, int steps, int *histt = NULL, int sw = 1);
-__device__ void helper_calculate_probability(int *hist, int total_pixels, double *prob);
-__device__ void helper_buildLook_up_table(double *prob, double *lut);
+// device function that extracts histogram of an image on gpu
+__device__ void
+helper_extract_histogram_rgb(const uchar* img,
+                             int* count,
+                             int x_start,
+                             int x_end,
+                             int y_start,
+                             int y_end,
+                             int width,
+                             int height,
+                             int steps,
+                             short channel,
+                             short channels_c = 3,
+                             int* histt = NULL,
+                             int sw = 1);
+__device__ void
+helper_extract_histogram(const uchar* img,
+                         int* count,
+                         int x_start,
+                         int x_end,
+                         int y_start,
+                         int y_end,
+                         int width,
+                         int height,
+                         int steps,
+                         int* histt = NULL,
+                         int sw = 1);
+__device__ void
+helper_calculate_probability(int* hist, int total_pixels, double* prob);
+__device__ void
+helper_buildLook_up_table(double* prob, double* lut);
 
-__global__ void extract_histogram_rgb(const uchar* img,  int *count, int x_start, int x_end, int y_start, int y_end, int width, int height, int steps, short channel, short channels_c = 3, int *histt = NULL, int sw = 1);
-__global__ void extract_histogram(const uchar* img, int *count, int x_start, int x_end, int y_start, int y_end, int width, int height, int steps, int *histt = NULL, int sw = 1);
-__global__ void calculate_probability(int *hist, int total_pixels, double *prob);
-__global__ void buildLook_up_table(double *prob, double *lut);
-__global__ void buildLook_up_table_rgb(int *hist_blue, int *hist_green, int *hist_red, int count, bool free_sw = true, double *lut_final = NULL, double *lut_blue = NULL, double *lut_green = NULL, double *lut_red = NULL);
-__global__ void lhe_build_luts(double ***all_luts, const uchar *img, int offset, int width, int height, int channel_c, int steps);
 __global__ void
-apply_interpolating_lhe(uchar* base,
-                        const uchar* img,
-                        int window,
-                        int offset,
-                        int width,
-                        int height,
-                        int channel_n,
-                        int steps,
-                        double*** all_luts);
-__global__ void apply_LHE(uchar *base, const uchar *img, int window, int width, int height, int steps, int channels_c);
-//end def gaurds
+extract_histogram_rgb(const uchar* img,
+                      int* count,
+                      int x_start,
+                      int x_end,
+                      int y_start,
+                      int y_end,
+                      int width,
+                      int height,
+                      int steps,
+                      short channel,
+                      short channels_c = 3,
+                      int* histt = NULL,
+                      int sw = 1);
+__global__ void
+extract_histogram(const uchar* img,
+                  int* count,
+                  int x_start,
+                  int x_end,
+                  int y_start,
+                  int y_end,
+                  int width,
+                  int height,
+                  int steps,
+                  int* histt = NULL,
+                  int sw = 1);
+__global__ void
+calculate_probability(int* hist, int total_pixels, double* prob);
+__global__ void
+buildLook_up_table(double* prob, double* lut);
+__global__ void
+buildLook_up_table_rgb(int* hist_blue,
+                       int* hist_green,
+                       int* hist_red,
+                       int count,
+                       bool free_sw = true,
+                       double* lut_final = NULL,
+                       double* lut_blue = NULL,
+                       double* lut_green = NULL,
+                       double* lut_red = NULL);
+
+// Macro for checking CUDA error codes and exiting if an error occured.
+#define CUDA_CHECK(condition)                                                  \
+  do {                                                                         \
+    cudaError_t error = condition;                                             \
+    if (error != cudaSuccess) {                                                \
+      fprintf(stderr,                                                          \
+              "CUDA error: %s:%d: %s \n",                                      \
+              __FILE__,                                                        \
+              __LINE__,                                                        \
+              cudaGetErrorString(error));                                      \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  } while (0)
+
+// Macro that has 6 inputs, 3 hists and uses cudaMemset to zero out them and
+// checks result using CUDA_CHECK.
+#define ZERO_OUT_RGB(histr, histg, histb)                                      \
+  do {                                                                         \
+    CUDA_CHECK(cudaMemset(histr, 0, sizeof(int) * 256));                       \
+    CUDA_CHECK(cudaMemset(histg, 0, sizeof(int) * 256));                       \
+    CUDA_CHECK(cudaMemset(histb, 0, sizeof(int) * 256));                       \
+  } while (0)
+
+#define ZERO_OUT_COUNTS(countr, countb, countg)                                \
+  do {                                                                         \
+    CUDA_CHECK(cudaMemset(countr, 0, sizeof(int)));                            \
+    CUDA_CHECK(cudaMemset(countb, 0, sizeof(int)));                            \
+    CUDA_CHECK(cudaMemset(countg, 0, sizeof(int)));                            \
+  } while (0)
+
+#define VALIDATE_KERNEL_CALL()                                                 \
+  do {                                                                         \
+    cudaError_t error = cudaGetLastError();                                    \
+    if (error != cudaSuccess) {                                                \
+      fprintf(stderr,                                                          \
+              "CUDA error: %s:%d: %s \n",                                      \
+              __FILE__,                                                        \
+              __LINE__,                                                        \
+              cudaGetErrorString(error));                                      \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  } while (0)
+
+// end def gaurds
 #endif // HELPERS_HPP
